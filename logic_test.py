8fb5ff88f2bc0e6b1e901af8bc3b0b58eee1e052
AntonBackeus <148983207+AntonBackeus@users.noreply.github.com>
2025-11-04 09:59:06 +0100

    Refactor test_dataset to use dataset fixture

--- logic_test.py
+++ logic_test.py
@@ -5,7 +5,7 @@ def dataset():
     """Return some data to test functions"""
     return {'data1': 1, 'data2': 2}
 
-def test_dataset():
+def test_dataset(dataset):
     """test and confirm fixture value"""
     assert dataset == {'data1': 1, 'data2': 2}
 
